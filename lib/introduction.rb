a7a7df4ba9cf7c5f44860b293fa0bcd994913e45
anya litvinova <anya.s.litvinova@gmail.com>
2019-08-10 20:04:42 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,7 +6,7 @@ def introduction_with_language(name, language)
   print "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def introduction_with_language_optional(name, language = Ruby)
+def introduction_with_language_optional(name, language = "Ruby")
   print "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
